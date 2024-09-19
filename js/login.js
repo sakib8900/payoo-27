4762933019461285893e8818cc6450e5cccc4597
sakib8900 <md.sakib388900@gmail.com>
2024-09-19 21:26:22 +0600

    ek jogot theka binno jogot entry

--- js/login.js
+++ js/login.js
@@ -1,14 +1,44 @@
-// console.log('button clicking file added');
+// // console.log('button clicking file added');
 
-// search: form submit reloading the page
+// // search: form submit reloading the page
 
-// step 1 set event handler
+// // step 1 set event handler
+// document.getElementById('button-login').addEventListener('click', function(event){
+//     // step-2 prevent default behavior (prevent reloading browser)
+//     event.preventDefault(); // <-----------vejal to beginners
+
+//     // step-3 get the phone number and the 
+//     const phoneNumber = document.getElementById('phone-number').value;
+//     const pinNumber = document.getElementById('pin-number').value
+
+//     console.log(phoneNumber, pinNumber);
+
+//     // step-4 validate phone and pin
+//     // This is temporary. You should do like this
+//     if(phoneNumber === '5' && pinNumber === '1234'){
+//         console.log('you are logged in');
+//         // step-5 allow user to use the website
+//     }
+//     else{
+//         alert('Wrong phone number or pin')
+//     }
+    
+// })
+
 document.getElementById('button-login').addEventListener('click', function(event){
-    // step-2 prevent default behavior (prevent reloading browser)
-    event.preventDefault(); // <-----------vejal to beginners
-    console.log('login button');
+    event.preventDefault();
 
-    // step-3 get the phone number
+    // get phone number and pin
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
-})
+    const pinNumber = document.getElementById('pin-number').value
+    console.log(phoneNumber, pinNumber);
+
+    // bad way to validate
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log('you are logged in bkash');
+        window.location.href = '/home.html'
+    }
+    else{
+        alert('Wrong phone number or email')
+    }
+});
